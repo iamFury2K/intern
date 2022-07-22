d2f8a436c4bd77eb4b82d9985e87d6ab4aa3e508
Muneeb <20bcs050@smvdu.ac.in>
2022-07-22 18:22:28 +0530

    ClinicalTrials_final/

---
+++ ClinicalTrials_final/code.py
@@ -0,0 +1,24 @@
+import requests
+import csv
+import json
+url = '''
+http://ClinicalTrials.gov/api/query/study_fields?expr=heart+attack&fmt=JSON&fields=NCTId,Condition,BriefTitle
+'''
+data = requests.get(url)
+data_dict = data.json()
+with open('data.csv', 'w') as f:
+    fieldname = ['APIVrs', 'DataVrs', 'Expression', 'NStudiesAvail', 'NStudiesFound', 'MinRank', 'MaxRank', 'NStudiesReturned', 'FieldList']
+    file_ = csv.DictWriter(f, fieldname, delimiter='-')
+    file_.writeheader()
+    data_mod = data_dict['StudyFieldsResponse'].items()
+    final_data = {}
+    counter = 0
+    for idx,i in enumerate(data_mod):
+        if idx < 9:
+            final_data.update({i[0] : i[1]})
+    file_.writerow(final_data)
+    
+    
+
+            
+
